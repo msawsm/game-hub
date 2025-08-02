ee82c330c6ed8ea67013ccbfb547eeafc486ee50
Muhammed Sameer <sameerbava30@gmail.com>
2025-08-02 12:08:27 +0530

    Create a custom hook to fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,32 +1,8 @@
-import apiClient from '@/services/api-client';
-import { useEffect, useState } from 'react'
-
-interface Game {
-    id: number;
-    name: string;
-}
-
-interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-}
+import useGames from '@/hooks/useGames';
 
 const GameGrid = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
-
-    useEffect(() => {
-        fetchGames();
-    }, []);
-
-    const fetchGames = async () => {
-        try {
-            const response = await apiClient.get<FetchGamesResponse>('/games');
-            setGames(response.data.results);
-        } catch (err) {
-            setError(err instanceof Error ? err.message : 'An error occurred while fetching games');
-        } 
-    };
+    const {games, error} = useGames();
+    
     if (error) return <div>Error: {error}</div>;
     return (
         games.map(game => (
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,50 @@
+import apiClient from "@/services/api-client";
+import { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+
+interface Game {
+    id: number;
+    name: string;
+}
+
+interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState('');
+
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const fetchGames = async () => {
+      try {
+        const response = await apiClient.get<FetchGamesResponse>('/games', { signal });
+        setGames(response.data.results);
+      } catch (err) {
+        if (err instanceof CanceledError) {
+          return; // Request was cancelled
+        }
+        else if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError('An error occurred while fetching games');
+        }
+      }
+    };
+
+    fetchGames();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return { games, error };
+}
+
+export default useGames;
