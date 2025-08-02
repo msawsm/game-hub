dd02af75c73c3d32b63bf97372e6d8cd38cf1fe7
Muhammed Sameer <sameerbava30@gmail.com>
2025-08-02 10:10:53 +0530

    Downgrade chakra ui to version 2 and build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 
 import './App.css'
-import { Button, HStack } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 
 function App() {
 
   return (
-    <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }} >
+      <GridItem area='nav' bg='red'>
+        Nav
+      </GridItem>
+      <Show above='lg'>
+        <GridItem area='aside' bg='gold'>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area='main' bg='dodgerblue'>
+        Main
+      </GridItem>
+    </Grid>
   )
 }
 
