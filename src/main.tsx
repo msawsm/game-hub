217fff054fbba34349b4ddadf8e5a5ebd65f893b
Muhammed Sameer <sameerbava30@gmail.com>
2025-08-02 09:12:00 +0530

    Install Chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+
+import { Provider } from "@/components/ui/provider"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -5,6 +7,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>,
 )
